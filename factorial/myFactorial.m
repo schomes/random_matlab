67b7e5c586672000fb90993deea36f73f3e7179f
David Schommer <djschomes@gmail.com>
2017-07-03 22:51:26 -0500

    Add initial factorial work

---
+++ factorial/myFactorial.m
@@ -0,0 +1,11 @@
+function [ answer ] = myFactorial( n )
+% Computes the factorial function n! = n * (n-1) * ... * 1
+
+% recursive solution to factorial goes here
+
+% answer = (put answer here)
+% Setting "answer" to the final value returns that value
+% "answer" is found above like this ----> function [answer] = ....
+
+end
+
